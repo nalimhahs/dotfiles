148bb4d2dff8d7d37afb23c35e296e9821f3c139
Milan Shah <milanshah1@gmail.com>
2025-06-26 22:44:50 +0530

    Initial commit

---
+++ brewfile.sh
@@ -0,0 +1,45 @@
+# Specify directory to install
+cask_args appdir: "/Applications"
+
+# Install packages
+brew 'mas'
+brew 'git'
+brew 'git-crypt'
+brew 'git-lfs'
+brew 'powerlevel10k'
+brew 'uv'
+brew 'mise'
+brew 'nvm'
+brew 'bitwarden-cli'
+brew 'fzf'
+brew 'zoxide'
+
+# Images, Video
+brew 'ffmpeg'
+
+# Fonts
+cask 'font-jetbrains-mono'
+cask 'font-hack-nerd-font'
+cask 'font-meslo-for-powerlevel10k'
+
+# Other apps
+cask 'visual-studio-code'
+cask 'google-chrome'
+cask 'thebrowsercompany-dia'
+cask 'ghostty'
+cask 'raycast'
+cask 'vlc'
+cask 'tailscale'
+cask 'signal'
+cask 'spotify'
+cask 'zed'
+cask 'zulu@17'
+cask 'ollama'
+cask 'orbstack'
+cask 'obsidian'
+cask 'bitwarden'
+cask 'macs-fan-control'
+
+## App Store apps
+# mas "Slack", id: 803453959
+mas "xcode" id: 497799835
